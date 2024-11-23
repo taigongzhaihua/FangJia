b6a64c841e45e228c6e8a9df64b2d53604bc4180
Peter Jones <944541960@qq.com>
2024-11-24 02:27:11 +0800

    新增 初始化导航服务，并注入到Viewmodel中

--- FangJia/ViewModels/MainWindowViewModel.cs
+++ FangJia/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using FangJia.Cores.Base;
+using FangJia.Cores.Interfaces;
 
 namespace FangJia.ViewModels;
 
 public class MainWindowViewModel : BaseViewModel
 {
+    public Dictionary<string, INavigationService> NavigationServices = [];
     public MainWindowViewModel()
     {
         Logger.Info("初始化主窗口viewmodel");
--- FangJia/Views/MainWindow.xaml.cs
+++ FangJia/Views/MainWindow.xaml.cs
@@ -1,10 +1,22 @@
-﻿namespace FangJia.Views;
+﻿using FangJia.Cores.Services;
+using FangJia.Cores.Services.NavigationServices;
+using FangJia.Models;
+using FangJia.ViewModels;
+
+namespace FangJia.Views;
 public partial class MainWindow
 {
 
     public MainWindow()
     {
         InitializeComponent();
+        var configService = new ConfigurationService("Configs/pagesConfig.json");
+        var pageConfigs = configService.GetConfig<PageConfig>("Pages");
+        var frameNavigationService = new FrameNavigationService(MainFrame, pageConfigs);
+        var viewModel = new MainWindowViewModel();
+        viewModel.NavigationServices.Add("Frame", frameNavigationService);
+        DataContext = viewModel;
+        frameNavigationService.NavigateTo("HomePage");
     }
 
 }
