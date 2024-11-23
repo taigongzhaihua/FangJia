d85a32b76aa8abe34e59e958f89f2a7f5ef14c14
Peter Jones <944541960@qq.com>
2024-11-24 00:22:32 +0800

    新增 创建主窗口与主页VM

---
+++ FangJia/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using FangJia.Cores.Base;
+
+namespace FangJia.ViewModels;
+
+public class MainWindowViewModel : BaseViewModel
+{
+    public MainWindowViewModel()
+    {
+        Logger.Info("初始化主窗口viewmodel");
+    }
+}
---
+++ FangJia/ViewModels/PageViewModels/HomeViewModel.cs
@@ -0,0 +1,8 @@
+﻿using FangJia.Cores.Base;
+
+namespace FangJia.ViewModels.PageViewModels
+{
+    public class HomeViewModel : BaseViewModel
+    {
+    }
+}
