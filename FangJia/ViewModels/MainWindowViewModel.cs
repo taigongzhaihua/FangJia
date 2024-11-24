b482407dbe6e097bc6cb67738f2e7384565708d3
Peter Jones <944541960@qq.com>
2024-11-24 23:32:46 +0800

    新增 实现侧边菜单数据绑定

--- FangJia/ViewModels/MainWindowViewModel.cs
+++ FangJia/ViewModels/MainWindowViewModel.cs
@@ -1,13 +1,31 @@
 ﻿using FangJia.Cores.Base;
 using FangJia.Cores.Interfaces;
+using FangJia.Cores.Services;
+using FangJia.Cores.Utils.Commands;
+using FangJia.Models;
+using FangJia.Models.ConfigModels;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
 
 namespace FangJia.ViewModels;
 
 public class MainWindowViewModel : BaseViewModel
 {
     public Dictionary<string, INavigationService> NavigationServices = [];
+    public ObservableCollection<MainMenuItemData> MenuItems { get; set; } = [];
     public MainWindowViewModel()
     {
         Logger.Info(message: "初始化主窗口viewmodel");
+
+        var configService = new ConfigurationService(configFilePath: Properties.Resources.MainPagesConfigUri);
+        var configs = configService.GetConfig<MainMenuItemConfig>("MainMenuItems");
+
+        foreach (var item in configs!)
+        {
+            ICommand command = new RelayCommand(o => NavigationServices["MainFrame"].NavigateTo(item.PageName));
+            MenuItems!.Add(new MainMenuItemData(item.Name, item.Icon, command));
+        }
+
     }
 }
