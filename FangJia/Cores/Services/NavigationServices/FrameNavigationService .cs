e965d4e6e5fd7f7ae9c71efb90e19f3ec279d15d
Peter Jones <944541960@qq.com>
2024-11-24 14:02:04 +0800

    重构 重构部分代码

--- FangJia/Cores/Services/NavigationServices/FrameNavigationService .cs
+++ FangJia/Cores/Services/NavigationServices/FrameNavigationService .cs
@@ -1,45 +1,79 @@
 ﻿using FangJia.Cores.Interfaces;
-using FangJia.Models;
+using FangJia.Models.ConfigModels;
+using System.Diagnostics.CodeAnalysis;
 using System.Windows.Controls;
 
 namespace FangJia.Cores.Services.NavigationServices;
 
+/// <summary>
+/// FrameNavigationService 类，用于管理 Frame 的导航功能。
+/// 提供页面导航、后退和前进的功能。
+/// </summary>
+[SuppressMessage("ReSharper", "UnusedMember.Global")]
 public class FrameNavigationService : INavigationService
 {
     private readonly Frame _frame;
     private readonly Dictionary<string, string> _pageMappings;
-    private readonly Stack<object> _navigationHistory;
 
+    /// <summary>
+    /// 构造函数，初始化 FrameNavigationService 实例。
+    /// </summary>
+    /// <param name="frame">用于导航的 Frame 对象。</param>
+    /// <param name="pageConfigs">页面配置的列表，用于映射页面名称到页面 URI。</param>
     public FrameNavigationService(Frame frame, List<PageConfig> pageConfigs)
     {
-        _frame = frame ?? throw new ArgumentNullException(nameof(frame));
-        _pageMappings = [];
-        _navigationHistory = new Stack<object>();
+        // 检查 frame 参数是否为 null
+        _frame = frame ?? throw new ArgumentNullException(paramName: nameof(frame));
 
+        // 初始化页面映射字典
+        _pageMappings = [];
+
+        // 根据页面配置列表填充页面映射字典
         foreach (var config in pageConfigs)
         {
-            _pageMappings[config.Name!] = config.Uri!;
+            _pageMappings[key: config.Name!] = config.Uri!;
         }
     }
 
+    /// <summary>
+    /// 导航到指定的视图。
+    /// </summary>
+    /// <param name="viewName">要导航到的视图名称。</param>
     public void NavigateTo(string viewName)
     {
-        if (_pageMappings.TryGetValue(viewName, out var uri))
+        // 根据视图名称查找对应的 URI 并导航
+        if (_pageMappings.TryGetValue(key: viewName, value: out var uri))
         {
-            _navigationHistory.Push(_frame.Content);
-            _frame.Navigate(new Uri(uri, UriKind.Relative));
+            _frame.Navigate(source: new Uri(uriString: uri, uriKind: UriKind.Relative));
         }
         else
         {
-            throw new ArgumentException($"View {viewName} not found in configuration.");
+            // 如果找不到视图，抛出异常
+            throw new ArgumentException(message: $"View {viewName} not found in configuration.");
         }
     }
 
+    /// <summary>
+    /// 导航回到前一个页面。
+    /// </summary>
     public void GoBack()
     {
+        // 如果可以后退，则执行后退操作
         if (_frame.CanGoBack)
         {
             _frame.GoBack();
+        }
+    }
+
+    /// <summary>
+    /// 导航到下一个页面。
+    /// </summary>
+    public void GoForward()
+    {
+        // 如果可以前进，则执行前进操作
+        if (_frame.CanGoForward)
+        {
+            _frame.GoForward();
         }
     }
 }
