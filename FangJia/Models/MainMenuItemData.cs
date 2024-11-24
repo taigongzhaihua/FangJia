e96b004781e77b9d7191b06bee3b16eff5b2fa6a
Peter Jones <944541960@qq.com>
2024-11-24 23:31:05 +0800

    新增 数据模型

---
+++ FangJia/Models/MainMenuItemData.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Input;
+
+namespace FangJia.Models
+{
+    public class MainMenuItemData(string? name, string? icon, ICommand command)
+    {
+        public string? Name { get; set; } = name;
+        public string? Icon { get; set; } = icon;
+        public ICommand? Command { get; set; } = command;
+    }
+}
