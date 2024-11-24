6abe40722af0c036e0e21d3362b22fde1844a92e
Peter Jones <944541960@qq.com>
2024-11-24 23:28:17 +0800

    新增 实现主页侧边菜单栏主要框架

--- FangJia/Views/Components/Controls/MainMenu.xaml.cs
+++ FangJia/Views/Components/Controls/MainMenu.xaml.cs
@@ -1,37 +1,33 @@
-﻿using System.Windows.Controls;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
+﻿using FangJia.Models;
+using System.Collections.ObjectModel;
+using System.Windows;
 
-namespace FangJia.Views.Components.Controls
+namespace FangJia.Views.Components.Controls;
+
+/// <summary>
+/// MainMenu.xaml 的交互逻辑
+/// </summary>
+public partial class MainMenu
 {
-    /// <summary>
-    /// MainMenu.xaml 的交互逻辑
-    /// </summary>
-    public partial class MainMenu
+
+    public MainMenu()
     {
-        public MainMenu()
-        {
-            InitializeComponent();
-        }
-        private void SidebarListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (SidebarListBox.SelectedItem is not ListBoxItem selectedItem) return;
-            // 获取选中项的索引
-            var selectedIndex = SidebarListBox.Items.IndexOf(selectedItem);
+        InitializeComponent();
+    }
 
-            // 计算选中项在列表中的目标位置
-            var targetTop = selectedIndex * selectedItem.ActualHeight;
+    public static readonly DependencyProperty MenuItemsProperty =
+        DependencyProperty.Register(
+            nameof(MenuItems),
+            typeof(ObservableCollection<MainMenuItemData>),
+            typeof(MainMenu),
+            new PropertyMetadata(
+                new ObservableCollection<MainMenuItemData>()
+                )
+            );
 
-            // 创建一个平滑移动的动画
-            DoubleAnimation animation = new DoubleAnimation
-            {
-                To = targetTop,
-                Duration = TimeSpan.FromMilliseconds(300),
-                EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseInOut }
-            };
-
-            // 应用动画到 Rectangle 的 TranslateTransform 的 Y 属性上
-            SelectionTransform.BeginAnimation(TranslateTransform.YProperty, animation);
-        }
+    public ObservableCollection<MainMenuItemData> MenuItems
+    {
+        get => (ObservableCollection<MainMenuItemData>)GetValue(MenuItemsProperty);
+        set => SetValue(MenuItemsProperty, value);
     }
 }
